b9dd280315566328ef24cf4b5c2c354576df5d49
Thomas L. <thomaslower9@gmail.com>
2025-05-09 15:15:53 +0100

    moving to bed to setup laptop os

---
+++ table.h
@@ -0,0 +1,18 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "column.h"
+#include "row.h"
+#include <stdlib.h>
+#include <vector>
+
+class Table
+{
+protected:
+std::vector
+
+public:
+    Table();
+};
+
+#endif // TABLE_H
