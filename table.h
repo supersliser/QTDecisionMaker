76f4cd4a389d622a0c2b4be34db758276193810b
Thomas L. <thomaslower9@gmail.com>
2025-05-09 15:16:23 +0100

    forgot to save first

--- table.h
+++ table.h
@@ -5,14 +5,33 @@
 #include "row.h"
 #include <stdlib.h>
 #include <vector>
+#include <string>
+
+struct Item {
+    std::string displayValue;
+    float worthValue;
+};
 
 class Table
 {
 protected:
-std::vector
+    std::vector<Column> m_headings;
+    std::vector<Row> m_rows;
+    std::vector<Item> m_items;
+    std::string m_title;
 
 public:
-    Table();
+    Table() = default;
+    ~Table() = default;
+
+    void addHeading(Column i_heading);
+    Column* heading(unsigned int i_index);
+    void addRow(Row i_row);
+    Row* row(unsigned int i_index);
+    void setTitle(std::string i_title);
+    std::string title() const;
+    Item* item(unsigned int i_index);
+    Item* item(unsigned int i_x, unsigned int i_y);
 };
 
 #endif // TABLE_H
