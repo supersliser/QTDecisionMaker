b7c60e5d86e2de0855ab087ddd3b1b63b25aaac1
Thomas Lower <thomaslower9@gmail.com>
2025-05-18 01:56:12 +0100

    bedding

---
+++ src/tableManager.cpp
@@ -0,0 +1,60 @@
+//
+// Created by t on 17/05/25.
+//
+
+#include "../include/tableManager.h"
+
+#include <fmt/format.h>
+#include <QHeaderView>
+
+tableManager::tableManager(QWidget* parent)
+    : QTableWidget(parent)
+{
+    connect(this, &QTableWidget::currentCellChanged, this, &tableManager::_selectItem);
+    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+}
+
+void tableManager::_selectItem(int row, int column, int prev_row, int prev_column)
+{
+    emit selectItem(row, column);
+}
+
+void tableManager::drawTable(Table& data)
+{
+    setColumnWidth(0, data.headingCount() + 1);
+    for (int i = 0; i < data.headingCount(); i++)
+    {
+        _setColumnHeader(i, tr(data.heading(i)->name().data()));
+    }
+    _setColumnHeader(data.headingCount(), tr("Total Value"));
+
+    data.calculateAllTotals();
+
+    setRowCount(data.rowCount());
+    for (int r = 0; r < data.rowCount(); r++)
+    {
+        _setItem(r, 0, tr(data.row(r)->name().data()));
+        for (int c = 1; c < data.headingCount(); c++)
+        {
+            _setItem(r, c, tr(data.item(c, r)->displayValue.data()));
+        }
+        _setItem(r, data.headingCount(), fmt::format("{:.2f}", data.row(r)->totalValue()).c_str());
+    }
+
+    show();
+}
+
+void tableManager::_setColumnHeader(int i_column, QString i_name)
+{
+    setHorizontalHeaderItem(i_column, new QTableWidgetItem(i_name));
+}
+
+void tableManager::_setItem(int i_row, int i_column, QString i_name)
+{
+    setItem(i_row, i_column, new QTableWidgetItem(i_name));
+}
+
+void tableManager::_setRowHeader(int i_row, QString i_name)
+{
+    setVerticalHeaderItem(i_row, new QTableWidgetItem(i_name));
+}
